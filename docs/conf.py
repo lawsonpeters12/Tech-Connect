9c9080915031a240dc476241c8804bd1ce90e6d3
lawsonpeters12 <101430705+lawsonpeters12@users.noreply.github.com>
2024-01-11 13:05:18 -0600

    Documentation Setup

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Tech Connect'
+copyright = '2024, Elena Simon, Lawson Peters, Michael Hebert, Nick Landry, Ryan Erbelding'
+author = 'Elena Simon, Lawson Peters, Michael Hebert, Nick Landry, Ryan Erbelding'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
