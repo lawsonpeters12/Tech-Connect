09a340ee03963fb24a13198b19e25f86973a3a97
ryanerbelding <77171786+ryanerbelding@users.noreply.github.com>
2024-01-12 06:56:43 -0600

    last push (hopefully)
    Formatted code and added homepage formatting for webscraped data to be put into

--- tech_connect/lib/main.dart
+++ tech_connect/lib/main.dart
@@ -2,16 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:tech_connect/pages/first_page.dart';
-// for debugging
-import 'package:html/parser.dart';
 
 void main() {
-  // for debugging until line 12
-  var document = parse(
-      '<body>Hello world! <a href="www.html5rocks.com">HTML5 rocks!');
-  print(document.outerHtml);
-
-   
   runApp(const MyApp());
 }
 
--- tech_connect/lib/pages/home_page.dart
+++ tech_connect/lib/pages/home_page.dart
@@ -13,10 +13,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> { 
     
   // Strings to store the extracted Article titles 
-  String result1 = 'Result 1'; 
-  String result2 = 'Result 2'; 
-  String result3 = 'Result 3'; 
-    
+  String event1 = 'event 1'; 
+  String event2 = 'event 2'; 
+  String event3 = 'event 3'; 
+  
+  String eventInfo1 = 'eventInfo1';
+  String eventInfo2 = 'eventInfo2';
+  String eventInfo3 = 'eventInfo3';
   // boolean to show CircularProgressIndication 
   // while Web Scraping awaits 
   bool isLoading = false; 
@@ -32,57 +35,80 @@ class _HomePageState extends State<HomePage> {
         
     // Getting the html document from the response 
       var document = parser.parse(response.body);
-      var elements = document.querySelectorAll('title');
+      //var elements = document.querySelectorAll('title');
      // print('something');
-      elements.forEach(print);
+      //elements.forEach(print);
       //document.outerHtml;
       //var document = response.body;
-      
-      var responseString1 = document 
+      /*
+      var responseEvent1 = document 
             .getElementsByClassName('lw_cal_upcoming_events'); 
   
-        print(responseString1);
+        print(responseEvent1);
 
       print(response.statusCode);
+      */
       //print(response.body);
       try { 
-          /*
-      // Scraping the first article title 
-        var responseString1 = document 
-            .getElementsByClassName('lw_cal_upcoming_events')[0] 
+      // Scraping the first event
+        var responseEvent1 = document 
+            .getElementsByClassName('lw_cal_event_list')[1] 
+            .children[3]; 
+        print(responseEvent1.text.trim()); 
+        
+        var responseInfo1 = document 
+            .getElementsByClassName('lw_events_time')[0] 
             .children[0] 
             .children[0] 
             .children[0]; 
   
-        print(responseString1.text.trim()); 
-          */
-      // Scraping the second article title 
-        var responseString2 = document 
-            .getElementsByClassName('articles-list')[0] 
+        print(responseInfo1.text.trim()); 
+          
+      // Scraping the second event
+        var responseEvent2 = document 
+            .getElementsByClassName('lw_events_title')[0] 
             .children[1] 
             .children[0] 
             .children[0]; 
   
-        print(responseString2.text.trim()); 
+        print(responseEvent2.text.trim()); 
+
+        var responseInfo2 = document 
+            .getElementsByClassName('lw_events_time')[0] 
+            .children[0] 
+            .children[0] 
+            .children[0]; 
+  
+        print(responseInfo2.text.trim()); 
           
-      // Scraping the third article title 
-        var responseString3 = document 
-            .getElementsByClassName('articles-list')[0] 
+      // Scraping the third event
+        var responseEvent3 = document 
+            .getElementsByClassName('lw_events_title')[0] 
             .children[2] 
             .children[0] 
             .children[0]; 
   
-        print(responseString3.text.trim()); 
+        print(responseEvent3.text.trim()); 
           
+        var responseInfo3 = document 
+            .getElementsByClassName('lw_events_time')[0] 
+            .children[0] 
+            .children[0] 
+            .children[0]; 
+  
+        print(responseInfo3.text.trim()); 
         // Converting the extracted titles into 
         // string and returning a list of Strings 
         return [ 
-          //responseString1.text.trim(), 
-          responseString2.text.trim(), 
-          responseString3.text.trim() 
+          responseEvent1.text.trim(), 
+          responseInfo1.text.trim(),
+          responseEvent2.text.trim(), 
+          responseInfo2.text.trim(),
+          responseEvent3.text.trim(),
+          responseInfo3.text.trim()
         ]; 
       } catch (e) { 
-        return ['', '', 'ERROR!']; 
+        return ['', '','ERROR!','', '','']; 
       } 
     } else { 
       return ['', '', 'ERROR: ${response.statusCode}.']; 
@@ -113,6 +139,7 @@ class _HomePageState extends State<HomePage> {
             child: Column( 
           mainAxisAlignment: MainAxisAlignment.center, 
           children: [ 
+            
               Container(color: Colors.white, child: Row(
                 children: [
                   SizedBox(height: 200,),
@@ -120,37 +147,43 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               ),
+              
               Container( color: Color.fromRGBO(198, 218, 231, 100),
                 child: Row(children: [
-                  SizedBox(height: 30,)
+                  SizedBox(height: 30),
+                  Text('current quarter')
                 ]),
               ),
+
             // if isLoading is true show loader 
             // else show Column of Texts 
             isLoading 
                 ? CircularProgressIndicator() 
                 : Container( color: Colors.white,
-                    child: Row( mainAxisAlignment: MainAxisAlignment.center,
+                    child: Row( mainAxisAlignment: MainAxisAlignment.start,
                       children: [ 
                       Column(
                         children: [
-                          Text(result1, 
+                          Text(event1, 
                           style: TextStyle( 
                               fontSize: 20, fontWeight: FontWeight.bold)), 
+                          Text(eventInfo1, style: TextStyle(fontSize:12, fontWeight: FontWeight.normal)),
                       SizedBox( 
                         
                         height: MediaQuery.of(context).size.height * 0.05, 
                         
                       ),
-                      Text(result2, 
+                      Text(event2, 
                           style: TextStyle( 
                               fontSize: 20, fontWeight: FontWeight.bold)), 
+                              Text(eventInfo2, style: TextStyle(fontSize:12, fontWeight: FontWeight.normal)),
                       SizedBox(
                         height: MediaQuery.of(context).size.height * 0.05, 
                       ), 
-                      Text(result3, 
+                      Text(event3, 
                           style: TextStyle( 
                               fontSize: 20, fontWeight: FontWeight.bold)), 
+                              Text(eventInfo3, style: TextStyle(fontSize:12, fontWeight: FontWeight.normal)),
                         ],
                       ),
                        
@@ -174,14 +207,19 @@ class _HomePageState extends State<HomePage> {
                 // displayed and making isLoading false 
                 // to hide the loader 
                 setState(() { 
-                  result1 = response[0]; 
-                  result2 = response[1]; 
-                  result3 = response[2]; 
+                  event1 = response[0]; 
+                  eventInfo1 = response[1];
+
+                  event2 = response[2];
+                  eventInfo2 = response[3];
+
+                  event3 = response[4];
+                  eventInfo3 = response[5]; 
                   isLoading = false; 
                 }); 
               }, 
               child: Text( 
-                'Scrap Data', 
+                'Get Events', 
                 style: TextStyle(color: Colors.white), 
               ), 
               color: Colors.blue, 
