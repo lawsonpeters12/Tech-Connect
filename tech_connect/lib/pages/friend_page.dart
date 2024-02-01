fa561ab033731df49140608695dc3ab5937ee4fa
elenasimon0409 <118309614+elenasimon0409@users.noreply.github.com>
2024-02-01 14:10:58 -0600

    search functionality
    Created the search functionality when the spy glass is tapped on the right side of the friends page. Has a random list of fruit to show functionality of search.

--- tech_connect/lib/pages/friend_page.dart
+++ tech_connect/lib/pages/friend_page.dart
@@ -26,8 +26,16 @@ class FriendPage extends StatelessWidget {
               fit: BoxFit.contain,
               height: 60,
             ),
-            SizedBox(width: 600),
-            Icon(Icons.search),
+            SizedBox(width: 500),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: (){
+                showSearch(
+                  context: context,
+                  delegate: MySearchDelegate(),
+                );
+              }
+            ),
             SizedBox(width: 20),
           ],
         ),
@@ -63,6 +71,69 @@ class FriendPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate {
+  List<String> searchResults = [
+      "Apple",
+      "Banana",
+      "Orange"
+    ];
+  
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+    icon: const Icon(Icons.arrow_back),
+    onPressed: () => close(context,null),
+  );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+    IconButton(
+      icon: const Icon(Icons.clear),
+      onPressed: (){
+        if (query.isEmpty){
+          close(context,null);
+        }
+        else{
+        query = '';
+        }
+      },
+    )
+  ];
+
+  @override
+  Widget buildResults(BuildContext context) => Center(
+    child: Text(
+      query,
+      style: const TextStyle(fontSize: 64, fontWeight: FontWeight.normal),
+    )
+  );
+
+  @override 
+  Widget buildSuggestions(BuildContext context){
+    List<String> suggestions = searchResults.where((searchResult){
+      final result = searchResult.toLowerCase();
+      final input = query.toLowerCase();
+
+      return result.contains(input);
+    }).toList();
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index){
+        final suggestion = suggestions[index];
+
+        return ListTile(
+          title: Text(suggestion),
+          onTap: (){
+            query = suggestion;
+
+            showResults(context);
+          }
+        );
+      },
     );
   }
 }
