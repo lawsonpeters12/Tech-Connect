c825dfbdf1906d5e5b28a92b17acf0d3460b1ad3
ryanerbelding <77171786+ryanerbelding@users.noreply.github.com>
2024-02-26 15:03:18 -0600

    a

--- tech_connect/lib/pages/user_page.dart
+++ tech_connect/lib/pages/user_page.dart
@@ -1,66 +1,51 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
+
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+//import 'package:tech_connect/components/click_button.dart';
+import 'package:tech_connect/pages/edit_user_page.dart';
+//import 'package:tech_connect/pages/register_page.dart';
 import 'package:tech_connect/user/appbar_widget.dart';
+import 'package:tech_connect/user/user_preferences.dart';
 import 'package:tech_connect/user/profile_widget.dart';
 import 'package:tech_connect/user/user.dart';
 import 'package:tech_connect/user/numbers_widget.dart';
-import 'package:tech_connect/pages/edit_user_page.dart';
+//import 'package:tech_connect/main.dart';
 
-class UserPage extends StatefulWidget {
-  const UserPage({Key? key}) : super(key: key);
+/*
+FirebaseAuth _auth = FirebaseAuth.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+*/
+
+class UserPage extends StatefulWidget{
+  const UserPage({super.key});
 
   @override
-  _UserPageState createState() => _UserPageState();
+  State<UserPage> createState() => _UserPageState();
 }
 
 class _UserPageState extends State<UserPage> {
-  late Future<UserInf> userFuture;
-  bool isDarkMode = false;
-  Color backgroundColor = Color.fromRGBO(198, 218, 231, 1);
+  // log user out
+  void logOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+// checks if the user doc exists
+/*
+// TODO: get access to fire store and work on this
+  Future checkUserInFirestore(User user, String username) async {
+    DocumentSnapshot doc = await _firestore.collection('users').doc(user.uid).get();
 
+    if(!doc.exists){
+      print('User Does Not Exist');
+    }
+  }
+  */
+// add logout button
   @override
-  void initState() {
-    super.initState();
-    userFuture = fetchUserData();
-  }
-
-  Future<UserInf> fetchUserData() async {
-    // Get the current user's email
-    User? currentUser = FirebaseAuth.instance.currentUser;
-    String userEmail = currentUser?.email ?? '';
-
-    // Retrieve the user document from Firestore based on the email
-    DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(userEmail)
-        .get();
-
-    // Extract user information from the document
-    Map<String, dynamic> userData = userSnapshot.data() as Map<String, dynamic>;
-    return UserInf(
-      imagePath: userData['profile_picture'] ?? 'https://firebasestorage.googleapis.com/v0/b/techconnect-42543.appspot.com/o/images%2Fdefault_user.PNG?alt=media&token=c592af94-a160-43c1-8f2b-29a7123756dd',
-      name: userData['name'] ?? '',
-      major: userData['major'] ?? '',
-      email: userEmail,
-      about: userData['about'] ?? '',
-    );
-  }
-
-  void editUserPage() {
-    // Navigate to the EditUserPage and pass a function to update user data
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => EditUserPage(updateUserData: updateUser)));
-  }
-
-  void updateUser(UserInf newUser) {
-    // Update user data and trigger a rebuild
-    setState(() {
-      userFuture = Future.value(newUser);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+    final user = UserPreferences.getUser();
+    
     return Scaffold(
       appBar: buildAppBar(context),
       backgroundColor: Color.fromRGBO(198, 218, 231, 100),
@@ -71,10 +56,7 @@ class _UserPageState extends State<UserPage> {
           ProfileWidget(
             imagePath: user.imagePath,
             onClicked: () async {
-                await Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => EditUserPage(),)
-              );
-              setState(() {});
+                
             },
           ),
           const SizedBox(height: 24),
@@ -89,39 +71,38 @@ class _UserPageState extends State<UserPage> {
   }
 
   Widget buildName(UserInf user) => Column(
-        children: [
-          Text(
-            user.name,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            user.major,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            user.email,
-            style: TextStyle(color: Colors.grey),
-          ),
-        ],
-      );
+    children: [
+      Text(
+        user.name,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+      ),
+      const SizedBox(height: 4),
+      Text(
+        user.major,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+      ),
+      const SizedBox(height: 4),
+      Text( 
+        user.email,
+        style:TextStyle(color: Colors.grey)
+      ),
+    ],
+  );
 
   Widget buildAbout(UserInf user) => Container(
-        padding: EdgeInsets.symmetric(horizontal: 48),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Bio',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              user.about,
-              style: TextStyle(fontSize: 16, height: 1.4),
-            )
-          ],
-        ),
-      );
+    padding: EdgeInsets.symmetric(horizontal: 48),
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        'Bio',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      const SizedBox(height: 16,),
+      Text(user.about,
+      style: TextStyle(fontSize:16, height: 1.4),
+      )
+    ],
+  )
+  );
 }
